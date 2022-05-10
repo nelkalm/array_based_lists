4bd2a1dec55440d5522245cceec15c31e8fc7b06
Nelson Lu <nelkalmlu@gmail.com>
2022-05-10 11:50:50 -0700

    Initial commit

---
+++ array_deque.py
@@ -0,0 +1,16 @@
+class ArrayDeque:
+
+    def __init__(self) -> None:
+        pass
+
+    def get_value(self, index):
+        pass
+
+    def set_value(self, index, value):
+        pass
+
+    def add_element(self, index, value):
+        pass
+
+    def remove_element(self, index):
+        pass
