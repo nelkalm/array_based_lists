4392743c393c730a7d4e42cf0c6c49cba7a6042d
Nelson Lu <nelkalmlu@gmail.com>
2022-05-23 15:12:34 -0700

    Commit with reverse_integer

---
+++ reverse_integer.py
@@ -0,0 +1,22 @@
+# Design an efficient algorithm to reverse a given integer.
+# For example if the input of the algorithm is 1234 then the output should be 4321.
+
+from audioop import reverse
+
+
+def reverse_integer(num):
+    num_string = str(num)
+    num_list = list(num_string)
+    first_index = 0
+    last_index = len(num_list) - 1
+
+    while first_index < last_index:
+        num_list[first_index], num_list[last_index] = num_list[last_index], num_list[first_index]
+        first_index += 1
+        last_index -= 1
+
+    return int(''.join(num_list))
+
+
+if __name__ == '__main__':
+    print(reverse_integer(1234))
