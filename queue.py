1088683c053cff0a02f9b75cef8a9f269ecc512c
Nelson Lu <nelkalmlu@gmail.com>
2022-05-30 13:42:19 -0700

    final commit

--- queue.py
+++ queue.py
@@ -9,13 +9,15 @@ class Queue:
     # O(1)
     def enqueue(self, data):
         self.queue.append(data)
-    
+
     # O(N). Linked list would be better here
     def dequeue(self):
-        data = self.queue[0]
-        del self.queue[0]
-        return data
-    
+        if self.size_queue() != 0:
+            data = self.queue[0]
+            del self.queue[0]
+            return data
+        return
+
     # O(1)
     def peek(self):
         return self.queue[0]
@@ -23,6 +25,7 @@ class Queue:
     # O(1)
     def size_queue(self):
         return len(self.queue)
+
 
 queue = Queue()
 queue.enqueue(1)
